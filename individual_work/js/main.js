e4a0f2e3246e9e935870f919acc03d02369bc763
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-04 01:30:23 +0200

    add all challenges & login form & notifications

--- individual_work/js/main.js
+++ individual_work/js/main.js
@@ -1,10 +1,22 @@
 "use strict"
-console.log("thy")
-let challenges = document.querySelectorAll(".challenges article");
+
+const challenges = document.querySelectorAll(".challenges article");
 
 challenges.forEach(element => {
-    let title = element.querySelector(".card__group--title");
+    const title = element.querySelector(".card__group--title");
     title.addEventListener("click", () => {
         element.classList.toggle("one-col");
     })
 });
+
+const notificationButton = document.querySelector("#notificationButton");
+const notifications = document.querySelector("#notifications")
+
+notificationButton.addEventListener("click", () => {
+    notifications.classList.toggle("open");
+})
+
+notifications.addEventListener("click", (e) => {
+    if(e.target != notifications) return;
+    notifications.classList.remove("open");
+})
